6027848697105c037d08eb57d6cb12784cd60d9a
yashila-b <154703713+yashila-b@users.noreply.github.com>
2025-03-21 21:41:30 +0000

    Coded Dice.cc

---
+++ Dice.cc
@@ -0,0 +1,22 @@
+#include "Dice.h"
+#include <stdlib.h>
+
+// Dice Ctor
+Dice::Dice() : {}
+
+// Roll function for Dice
+void Dice::roll() {
+    dice1 = rand() % 6 + 1;
+    dice2 = rand() % 6 + 1;
+}
+
+// Returns sum of the 2 dices
+int Dice::add() {
+    return dice1 + dice2;
+}
+
+// Checks if both dice equal
+bool Dice::isEqual() {
+    return dice1 == dice2;
+}
+
