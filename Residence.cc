fef2b44c936d1bfdbf916e367b77281d370c32f5
d47kumar <157913766+d47kumar@users.noreply.github.com>
2025-03-22 13:29:12 +0000

    Updated Residence class

---
+++ Residence.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include "Residence.h"
+#include "Player.h"
+using namespace std;
+
+// Constructor for Residence
+Residence::Residence(const std::string name, const int index, const bool isProperty, const int cost, const bool isAcademic) :
+Property{name, index, true, 200, false} {}
+
+// Destructor for Residence
+Residence::~Residence() {}  
+
+// Returns the rent of the residence
+int Residence::getRent() const {
+    return rent;
+} // Residence::getRent
+
+// Performs the action of the residence
+void Residence::performAction(Player* player) const {
+    // If the property is owned
+    Player* owner = getOwner();
+    if (owner != nullptr) {
+        if (owner != player) {
+            // Calculate the rent
+            int rent = getRent();
+            // Pay the rent
+            player->subtractMoney(rent);
+            owner->addMoney(rent);
+        } 
+    } 
+}
--- Residence.h
+++ Residence.h
@@ -6,5 +6,23 @@
 #include "Player.h"
 
 // Residence class
+class Residence : public Property {
+    const int cost = 200;                  // Cost of the residence
+    const int rent = 25;                   // Rent of the residence
+
+    public: 
+        // Constructor for Residence
+        Residence(const std::string name, const int index, const bool isProperty, const int cost, const bool isAcademic);
+
+        // Destructor for Residence
+        ~Residence();
+
+        // Getters
+        // Returns the rent of the residence
+        int getRent() const;
+
+        // Performs the action of the residence
+        void performAction(Player* player) const override;
+};
 
 #endif
