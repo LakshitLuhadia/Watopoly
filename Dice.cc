3e85a4f5f6c10a5c440ec7dbcb29b08cc28cbe0d
yashila-b <154703713+yashila-b@users.noreply.github.com>
2025-03-23 21:59:01 +0000

    Dice added testing

--- Dice.cc
+++ Dice.cc
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 
 // Dice Ctor
-Dice::Dice() {
+Dice::Dice(bool testing) {
     dice1 = 0;
     dice2 = 0;
+    testing = testing;
 }
 
 // Roll function for Dice
-void Dice::roll() {
-    dice1 = rand() % 6 + 1;
-    dice2 = rand() % 6 + 1;
+void Dice::roll(int d1 = -1, int d2 = -1) {
+    if (testing && d1 > -1 && d2 > -1) {
+        dice1 = d1;
+        dice2 = d2;
+    } else {
+        dice1 = rand() % 6 + 1;
+        dice2 = rand() % 6 + 1;
+    }
 }
 
 // Returns sum of the 2 dices
--- Dice.h
+++ Dice.h
@@ -5,13 +5,14 @@
 class Dice {
     static int dice1; // Dice 1's roll (ranges from 1 - 6)
     static int dice2; // Dice 2's roll (ranges from 1 - 6)
+    static bool testing; // Whether in testing mode or not
 
     public:
         // Ctor for Dice
-        Dice();
+        Dice(bool testing);
 
         // Rolls the dice (sets dice1 and dice2 to numbers from 1 - 6)
-        static void roll();
+        static void roll(int d1 = -1, int d2 = -1);
 
         // Adds dice1 and dice2 values (ranges 1 - 12)
         static int add();
